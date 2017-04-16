4b5eaf496e2f4b7857e3e1ac8eda6a618019ed88
Suneil Basu <suneil@futureprogress.com>
2017-04-16 23:26:09 +0100

    Add not working username form

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"strings"
 )
+
+const formHTML = `<html>
+<head>
+<title></title>
+</head>
+<body>
+<form action="/senddatahere" method="post">
+    Username:<input type="text" name="username">
+    Password:<input type="password" name="password">
+    <input type="submit" value="Login">
+</form>
+</body>
+</html>
+`
+
+var formTemplate = template.Must(template.New("form").Parse(formHTML))
 
 var names = []string{
 	"George",
@@ -17,13 +35,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
-	names = append(names, "Fred")
+	urlBits := strings.Split(r.URL.Path, "/")
+	name := urlBits[len(urlBits)-1]
+
+	names = append(names, name)
 	fmt.Fprintf(w, "%d", len(names))
+}
+
+func loginForm(w http.ResponseWriter, r *http.Request) {
+	formTemplate.Execute(w, nil)
+}
+
+func sendDataHere(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Fprintf(w, "username: %s", r.Form["username"])
+	fmt.Fprintf(w, "password: %s", r.Form["password"])
 }
 
 func main() {
 	fmt.Println("Trying to run")
-	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/senddatahere/", sendDataHere)
+	http.HandleFunc("/login/", loginForm)
+	http.HandleFunc("/add/", addHandler)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
